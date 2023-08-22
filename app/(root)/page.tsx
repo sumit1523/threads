fcfc22d5490ece06c72d0dbb1733483d31124d98
sumit kumar <sumit1523@gmail.com>
2023-08-23 01:55:16 +0530

    Assets and Layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <h1>Home</h1>
     </div>
   )
 }
---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Topbar =()=>{
+    return (
+    <nav className="topbar">
+        <Link href="/" className="flex ite gap-4" >
+        <Image src="/log.png" alt="logo" width={28} height={28}/>
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+    </Link></nav>
+    )
+    }
+    export default Topbar
